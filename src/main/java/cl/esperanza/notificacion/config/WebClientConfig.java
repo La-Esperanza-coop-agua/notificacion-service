6e31fdb28c2fa56631f579e71880c26f505fa189
CyCklop <ignacioandresflores06@gmail.com>
2026-07-02 11:26:17 -0400

    modificado el config

--- src/main/java/cl/esperanza/notificacion/config/WebClientConfig.java
+++ src/main/java/cl/esperanza/notificacion/config/WebClientConfig.java
@@ -3,12 +3,13 @@ package cl.esperanza.notificacion.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class WebClientConfig {
-    
-    @Bean(name = "webClientSocios") 
-    public WebClient webClientSocios() {
-        return WebClient.builder().baseUrl("http://localhost:8082/api/v1/socios").build();
+
+    @Bean
+    public WebClient sociosWebClient(@Value("${socios.service.url:http://localhost:8082/api/v1/socios}") String sociosServiceUrl) {
+        return WebClient.builder().baseUrl(sociosServiceUrl).build();
     }
 }
